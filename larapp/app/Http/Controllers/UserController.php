bbfbfc57e4c4e10c5d79694121ca82844badbf08
Carlos Andrés Rojas Parra <carlos.rojasp@autonoma.edu.co>
2020-10-22 19:38:58 -0500

    Bug fixes

--- larapp/app/Http/Controllers/UserController.php
+++ larapp/app/Http/Controllers/UserController.php
@@ -41,7 +41,25 @@ class UserController extends Controller
      */
     public function store(UserRequest $request)
     {
-        //
+        $user = new User;
+        $user->fullname  = $request->fullname;
+        $user->email     = $request->email;
+        $user->phone     = $request->phone;
+        $user->birthdate = $request->birthdate;
+        $user->gender    = $request->gender;
+        $user->address   = $request->address;
+        if ($request->hasFile('photo')) {
+            $file = time().'.'.$request->photo->extension();
+            $request->photo->move(public_path('imgs'), $file);
+            $user->photo = 'imgs/'.$file;
+}
+        $user->password  = $request->password;
+        
+        if($user->save()) {
+            return redirect('users')->with('message', 'El Usuario: '.$user->fullname.' fue Adicionado con Exito!');
+        }
+        $user->password  = bcrypt($request->password);
+
     }
 
     /**
@@ -52,7 +70,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return view('users.show')->with('user',$user);   
     }
 
     /**
@@ -63,7 +81,7 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        return view('users.edit')->with('user',$user);   
     }
 
     /**
